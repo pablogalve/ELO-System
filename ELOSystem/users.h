2c0e135570d295c09a62bfecee03506c02875d57
Pablo <eduinvestor100@gmail.com>
2019-05-25 16:35:45 +0200

    functions base

--- ELOSystem/users.h
+++ ELOSystem/users.h
@@ -1,19 +1,28 @@
-#ifndef _USERS_H
-#define _USERS_H
+#ifndef _ELOMANAGER_H
+#define _ELOMANAGER_H
 #include <iostream>
 using namespace std;
 #include <string>
 
-class userController {
-private:
+#define K 5; //constant number used to calculate ELO variations
+
+struct users {
 	string username;
 	float ELO;
 	float score;
+};
+class ELOManager {
+private:
+	
 public:
-	userController();
-	~userController();
+	ELOManager(); //Constructor
+	~ELOManager(); //Destructor
 	void calculateELO();
-
+	float getAvgUp(); //Get average ELO of all the users above you
+	float getAvgDown(); //Get average ELO of all the users below you
+	float getProbWinUp(); //Get your probabilities of beating users above you
+	float getProbWinDown(); //Get your probabilities of beating users below you
+	void 
 };
 
-#endif // !_USERS_H
+#endif // !_ELOMANAGER_H
