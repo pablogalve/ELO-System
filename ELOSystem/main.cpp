845c59302f357e93fffd23dc4b54ce22f62bfa78
Pablo <eduinvestor100@gmail.com>
2019-06-02 23:29:39 +0200

    project backup working for laptop

---
+++ ELOSystem/main.cpp
@@ -0,0 +1,39 @@
+#include "users.h"
+
+
+int main() {
+	ELOManager* elo = new ELOManager();
+	string myName;
+
+	cout << "This is my ELO system :)" << endl;
+	cout << "What is your name?";
+	cin >> myName;
+
+	//Add users
+	elo->addNewUser(myName, 10);
+	elo->addNewUser("User1", 80);
+	elo->addNewUser("User2", 30);
+	elo->addNewUser("User3", 90);
+	elo->addNewUser("User4", 100);
+	elo->addNewUser("User5", 85);
+	elo->addNewUser("User6", 40);
+	elo->addNewUser("User7", 50);
+	elo->addNewUser("User8", 70);
+
+	//elo->addNewUser("first", 1);
+	//for (int i = 0; i < 10; i++) {
+		//elo->addNewUser("user", i);
+	//}
+
+
+	cout << "-- Users --" << endl;
+	elo->printUsers();
+	cout << "-- Sorting users by ELO... " << endl;
+	elo->quickSort();
+	//elo->orderList();
+	//elo->test();
+	elo->printUsers();
+	cout << "Done! " << endl;
+
+	return 0;
+}
---
+++ ELOSystem/users.h
@@ -0,0 +1,49 @@
+#ifndef _USERS_H
+#define _USERS_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define K 5; //constant number used to calculate ELO variations
+
+struct user {
+public:
+	string username;
+	float ELO = NULL; //Score of the user
+	float score = NULL; //Result of the game
+	float exp = NULL; //Level of mastery. 1 victory = +3. 1 draw = 2. 1 loss = +1
+	user* next = nullptr;
+	user* previous = nullptr;
+};
+class ELOManager {
+private:
+	user* first;
+	user* last;
+public:
+	ELOManager(); //Constructor
+	~ELOManager(); //Destructor
+
+	void calculateELO();
+	float getAvgUp(); //Get average ELO of all the users above you
+	float getAvgDown(); //Get average ELO of all the users below you
+	float getProbWinUp(); //Get your probabilities of beating users above you
+	float getProbWinDown(); //Get your probabilities of beating users below you
+	void calculateScore(); //Calculate the resulting score of a game/day 
+
+	//Normal functions
+	void addNewUser(string name, float ELO); //Add a new user with an ELO of 0	
+	void printUsers();
+
+	//Ordering users by ELO
+	int getArraySize(); //Returns the number of users
+	void quickSort(); //Order users based on their ELO
+	void _quickSortRecursive(user* low, user* high);
+	user* partition(user* low, user* high);
+	void swap(user* name, user* second); //change the position of two users
+	user* getFirstUser(); //Returns the first user
+	void orderList(); //Orders the list - Beta mode. It is better to use quicksort method 
+	int areTheyNeighbours(user* A, user* B);
+	void test();
+};
+
+#endif // !_USERS_H
