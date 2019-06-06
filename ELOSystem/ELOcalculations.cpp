9d2fb430aae1976c49de890e75da8ffc509f5195
Pablo <eduinvestor100@gmail.com>
2019-06-06 16:12:53 +0200

    ELO internal calculations

--- ELOSystem/ELOcalculations.cpp
+++ ELOSystem/ELOcalculations.cpp
@@ -1,7 +1,28 @@
 #include "users.h"
 
 void ELOManager::calculateELO(user* myself, float result) {
-	myself->ELO += (result - getProbWin(myself->ELO, myself->next->ELO)) * myself->next->ELO * K;
+	float changeELO;
+	float adjust;
+	changeELO = (result - getProbWin(myself->ELO, myself->next->ELO)) * myself->next->ELO * K;
+	
+	//Change ELO balances to make sure that there are not negative ELOs and to create inflation on users with low ELO
+	if (result == 1) {
+		if (myself->ELO < 100) //Bonus to help users grow from low ELOs 
+			changeELO *= 1.5; //50% bonus
+		else if (myself->ELO < 200)
+			changeELO *= 1.2; //20% bonus
+
+		myself->ELO += changeELO;
+	}
+	else { 
+		adjust = changeELO + myself->ELO;
+		if (adjust < 0) //Players can't have negative ELO
+			myself->ELO = 0;
+		if (adjust <= 100)
+			changeELO += 20; //Bonus to avoid users approach a very low level
+
+
+	}
 }
 void ELOManager::calculateScore() {
 	//This must be changed at a later stage
